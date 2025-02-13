82d82bf0d3fbc4dd93f0b8e84adde083c1235a80
Theewebwizard <thewebwizard12@gmail.com>
2025-02-14 01:42:29 +0530

    Live suggestions completed

--- src/components/Molecules/Boxlayer.jsx
+++ src/components/Molecules/Boxlayer.jsx
@@ -2,7 +2,7 @@ import Box from "../Atoms/Box"
 
 const Boxlayer = () => {
   return (
-    <div className="bg-red-500 gap-[7rem] w-full h-auto justify-center flex flex-row ml-[0.1rem] mt-[3rem]">
+    <div className="bg-red-500 gap-[7rem] w-full h-auto justify-center flex flex-row ml-[0.1rem] mt-[2rem]">
       <Box divname="meow" color="bg-gray-900" />
       <Box divname="meow" color="bg-gray-900" />
       <Box divname="meow" color="bg-gray-900" />
--- src/components/Molecules/Middlefile.jsx
+++ src/components/Molecules/Middlefile.jsx
@@ -1,14 +1,20 @@
 const Middlefile = () => {
   return (
-    <div className="bg-red-500 w-30rem h-[20rem] flex">
-      <div className="">
-
+    <div className="bg-red-500 rounded-xl mt-[2rem] w-[99.8%] ml-[0.1rem] h-[30rem] flex">
+      
+      <div className="bg-amber-300 w-[63%] rounded-xl h-[98%] ml-[0.2rem] mt-[0.3rem] flex-row gap-[2rem]">
+          <div className="flex w-full gap-2">
+              <div className="flex bg-gray-500 mt-[0.8rem] h-[3rem] w-[92%] ml-[1rem] text-3xl items-center rounded-xl">
+            LIVE SUGGESTIONS
+              </div>
+              <div className="h-[3rem] rounded-xl bg-gray-500 mt-[0.8rem]">
+                meow
+              </div>
+          </div>
+          <div className="mt-[0.6rem] w-[96.5%] rounded-xl h-[84%] ml-[0.9rem] bg-red-950 ">
+           clasmate 
+          </div>       
       </div>
-
-      <div>
-
-      </div>
-
     </div>
   )
 }
